c27a69f438a343afe8260b8295f912c47a24e48e
lixin-lovezx <75163596+lixin-lovezx@users.noreply.github.com>
2021-09-26 10:06:53 +0800

    。。。

---
+++ src/mobiletrain/web/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.mobiletrain.web;
+
+
+Public HelloServlet{
+	public class LoginServlet extends HttpServlet {
+		protected void doGet(HttpServletRequest request, HttpServletResponse response){
+			
+		}
+	
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+		}
+	
+	}
+
+}
