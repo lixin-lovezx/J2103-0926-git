0b586d48e8f83f0d9e3b37bbbd63591afadd21ba
lixin-lovezx <75163596+lixin-lovezx@users.noreply.github.com>
2021-09-26 10:52:34 +0800

    Update HelloServlet.java
    ...

--- src/mobiletrain/web/HelloServlet.java
+++ src/mobiletrain/web/HelloServlet.java
@@ -5,6 +5,7 @@ Public HelloServlet{
 	public class LoginServlet extends HttpServlet {
 		protected void doGet(HttpServletRequest request, HttpServletResponse response){
 			System.out.println("1234");
+			System.out.println("1234");
 		}
 	
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
